cd62c19ddef60018dc868d30cc6b991bc95b58f0
ariana124 <ariana.bibiano@gmail.com>
2020-08-07 21:24:46 -0700

    added body parser to parse through form data and actual

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// urlencoded parses data that comes from an html form.
+// extended: true allows for nested objects from post, need to explicitly declare this because it's good practice.
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
     // __dirname is going to give us the filepath of the current file no matter where it's hosted.
@@ -8,7 +13,13 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    res.send("Thanks for sending that!");
+
+    let num1 = parseInt(req.body.num1);
+    let num2 = parseInt(req.body.num2);
+
+    let result = num1 + num2;
+
+    res.send(`The result is ${result}`);
 });
 
 app.listen(3000, function(){
